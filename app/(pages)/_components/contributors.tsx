d1c65b6f994294f951a9cb500a9f3e57a25a2cb9
JohnTrunix <john@trunix.ch>
2024-06-22 01:08:30 +0200

    fix(about): fix hydration error when calling github api on client side

---
+++ app/(pages)/_components/contributors.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import useSWR from "swr";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+
+export interface Contributor {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  contributions: number;
+}
+
+const fetchContributors = (url: string) => {
+  return fetch(url, {
+    headers: {
+      Accept: "application/vnd.github+json",
+      "X-GitHub-Api-Version": "2022-11-28"
+    }
+  }).then((res) => {
+    if (!res.ok) {
+      console.warn("Failed to fetch contributors", res);
+      return [] as Contributor[];
+    } else {
+      return res.json() as Promise<Contributor[]>;
+    }
+  });
+};
+
+export const Contributors = () => {
+  const { data, error } = useSWR(
+    "https://api.github.com/repos/theorem-dev/theorem/contributors",
+    fetchContributors
+  );
+
+  if (error) return null;
+  if (!data) return null;
+
+  return (
+    <div className="w-full flex flex-wrap gap-2">
+      {data.map((contributor) => (
+        <a href={contributor.html_url} target="_blank" key={contributor.login}>
+          <TooltipProvider delayDuration={200}>
+            <Tooltip>
+              <TooltipTrigger>
+                <Avatar className="border dark:bg-muted-foreground">
+                  <AvatarImage src={contributor.avatar_url} />
+                  <AvatarFallback>
+                    {contributor.login.substring(0, 2)}
+                  </AvatarFallback>
+                </Avatar>
+              </TooltipTrigger>
+              <TooltipContent side="bottom">
+                {contributor.login} - {contributor.contributions}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </a>
+      ))}
+    </div>
+  );
+};
--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,22 +1,8 @@
-"use client";
-
-import { use } from "react";
-import { fetchGithubContributors, Contributor } from "@/lib/github-api";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger
-} from "@/components/ui/tooltip";
 import { Button } from "@/components/ui/button";
 import { SiGithub } from "@icons-pack/react-simple-icons";
-
-const contributorsData = fetchGithubContributors("theorem-dev", "theorem");
+import { Contributors } from "../_components/contributors";
 
 export default function AboutPage() {
-  const contributors = use(contributorsData);
   return (
     <div className="flex flex-col items-start justify-start flex-1 px-6 pb-10 gap-y-5 md:w-[80%] md:mx-auto">
       <h2 className="text-2xl font-bold md:text-3xl">About</h2>
@@ -46,41 +32,13 @@ export default function AboutPage() {
         idea for new features or improvements is welcome. Feel free to open an
         issue for discussion.
       </p>
+      <Contributors />
       <Button variant="outline" size="default" asChild>
         <a href="https://github.com/theorem-dev/theorem" target="_blank">
           <SiGithub className="h-6 w-6 mr-2" />
           Theorem GitHub
         </a>
       </Button>
-      <div className="w-full flex flex-wrap gap-2">
-        {contributors.map((contributor) => (
-          <a
-            href={contributor.html_url}
-            target="_blank"
-            key={contributor.login}
-          >
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger>
-                  <Avatar className="border dark:bg-muted-foreground">
-                    <AvatarImage src={contributor.avatar_url} />
-                    <AvatarFallback>
-                      {contributor.login.substring(0, 2)}
-                    </AvatarFallback>
-                  </Avatar>
-                </TooltipTrigger>
-                <TooltipContent side="bottom">
-                  {contributor.login} - {contributor.contributions}
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </a>
-        ))}
-      </div>
     </div>
   );
 }
-
-/*
-
-*/
