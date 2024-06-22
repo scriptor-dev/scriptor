01dcc59653cb9ace8d04ba5f38aba935a304e2d3
JohnTrunix <john@trunix.ch>
2024-06-22 02:43:45 +0200

    test(e2e): add navigation test

---
+++ cypress/e2e/navigation.cy.ts
@@ -0,0 +1,64 @@
+describe("Navigation", () => {
+  context("Desktop resolution", () => {
+    beforeEach(() => {
+      cy.viewport(1280, 800);
+    });
+
+    it("should navigate to the /theorems page", () => {
+      cy.visit("http://localhost:3000/");
+      cy.get('a[href*="theorems"]').click();
+      cy.url().should("include", "/theorems");
+      cy.get("h2").contains("Theorems");
+    });
+
+    it("should navigate to the /statistics page", () => {
+      cy.visit("http://localhost:3000/");
+      cy.get('a[href*="statistics"]').click();
+      cy.url().should("include", "/statistics");
+      cy.get("h2").contains("Statistics");
+    });
+
+    it("should navigate to the /about page", () => {
+      cy.visit("http://localhost:3000/");
+      cy.get('a[href*="about"]').click();
+      cy.url().should("include", "/about");
+      cy.get("h2").contains("About");
+    });
+
+    it("should navigate to the /404 page", () => {
+      cy.visit("http://localhost:3000/not-existing-page", {
+        failOnStatusCode: false
+      });
+      cy.get("h1").contains("404 - Page Not Found");
+    });
+
+    it("should navigate to the / page", () => {
+      cy.visit("http://localhost:3000/about");
+      cy.get('a[href="/"]').contains("Home").click();
+      cy.url().should("eq", "http://localhost:3000/");
+    });
+
+    it("should navigate from logo to the / page", () => {
+      cy.visit("http://localhost:3000/about");
+      cy.get('a[href="/"]').get("p").contains("Theorem").click();
+      cy.url().should("eq", "http://localhost:3000/");
+    });
+  });
+
+  context("Mobile resolution", () => {
+    beforeEach(() => {
+      cy.viewport(390, 844);
+      cy.visit("http://localhost:3000/");
+      cy.get("button").find("svg.lucide.lucide-menu").parents("button").click();
+    });
+
+    it("should open side menu", () => {
+      cy.get('a[href="/"]').get("p").contains("Theorem");
+      cy.get("button").find("svg.lucide.lucide-x");
+      cy.get("a").contains("Home");
+      cy.get("a").contains("Theorems");
+      cy.get("a").contains("Statistics");
+      cy.get("a").contains("About");
+    });
+  });
+});
