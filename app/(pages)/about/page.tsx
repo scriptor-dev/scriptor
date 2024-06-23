07fe603e13380a4040d60fa76f5376591a0bae37
JohnTrunix <john@trunix.ch>
2024-06-23 15:24:49 +0200

    feat(about): add faqs

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,30 +1,43 @@
-import { Button } from "@/components/ui/button";
 import { SiGithub } from "@icons-pack/react-simple-icons";
+
+import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from "@/components/ui/accordion";
+
 import { Contributors } from "../_components/contributors";
 
 export default function AboutPage() {
   return (
     <div className="flex flex-col items-start justify-start flex-1 px-6 pb-10 gap-y-5 md:w-[80%] md:mx-auto">
       <h2 className="text-2xl font-bold md:text-3xl">About</h2>
-      <p className="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      <h3 className="text-xl font-bold md:text-2xl">Sponsors</h3>
-      <p className="text-justify">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>What is a Theorem?</AccordionTrigger>
+          <AccordionContent>
+            A theorem is a mathematical statement that has been proven true
+            based on previously established statements, such as other theorems
+            or axioms.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Who is behind Theorem?</AccordionTrigger>
+          <AccordionContent>
+            Theorem was initiated by the DS22-t class of the Bachelor program
+            Data Science at the University of Applied Sciences ZHAW.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>How can I support Theorem?</AccordionTrigger>
+          <AccordionContent>
+            The app was built and tested by volunteers. We would be delighted if
+            you would simply use the app and give us some brief feedback.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
       <h3 className="text-xl font-bold md:text-2xl">Contributors</h3>
       <p className="text-justify">
         Theorem is an Open Source project and every contribution is welcome!
